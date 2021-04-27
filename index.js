ab3dd23b18d8913ebf85ac6f56c7a3f4f318b3d5
Avraz  Zebary <avrazebary@gmail.com>
2021-04-27 14:29:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your code here
+const submitData = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body:
+  {
+    }/* Your data goes here */
+    name: "Avraz",
+    email: "avrazebary@gmail.com"
+};
